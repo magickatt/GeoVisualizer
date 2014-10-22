66a2b963faf98c90fa54da9ef921e5ad2d3efd6d
Andrew Kirkpatrick <development@andrew-kirkpatrick.com>
2014-10-22 19:33:00 +0100

    Adding basic PHPUnit

---
+++ tests/unit/SampleTest.php
@@ -0,0 +1,10 @@
+<?php
+
+class SampleTest extends PHPUnit_Framework_TestCase
+{
+    public function testSomething()
+    {
+        // Optional: Test anything here, if you want.
+        $this->assertTrue(TRUE, 'This should already work.');
+    }
+}
